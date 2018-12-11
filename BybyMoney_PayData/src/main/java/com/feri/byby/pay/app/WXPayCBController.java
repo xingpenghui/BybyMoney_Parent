cdf8374fbc200861af16e14297078436afd94ffb
邢朋辉 <xingfei_work@163.com>
2018-12-11 18:22:49 +0800

    实现BybyMoney的支付的回调接口

--- BybyMoney_PayData/src/main/java/com/feri/byby/pay/app/WXPayCBController.java
+++ BybyMoney_PayData/src/main/java/com/feri/byby/pay/app/WXPayCBController.java
@@ -1,9 +1,15 @@
 package com.feri.byby.pay.app;
 
+import com.feri.byby.pay.util.WXPayUtil;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *@Author feri
@@ -12,7 +18,21 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class WXPayCBController {
     @RequestMapping("/wxpaynotify.do")
-    public void notify(HttpServletRequest request,HttpServletResponse response){
+    public void notify(HttpServletRequest request,HttpServletResponse response) throws Exception {
+        //获取回调数据
+        String res=IOUtils.toString(request.getInputStream(),"UTF-8");
+        Map<String,String> map=WXPayUtil.xmlToMap(res);
+        //修改数据库中的订单状态
+        //支付状态
+        String status=map.get("result_code");
+        //支付完成时间
+        String wcTime=map.get("time_end");
 
+
+        Map<String,String> repData=new HashMap<>();
+        repData.put("return_code","SUCCESS");
+        repData.put("return_msg","OK");
+        String repXml=WXPayUtil.mapToXml(repData);
+        response.getWriter().print(repXml);
     }
 }
--- BybyMoney_PayData/src/main/java/com/feri/byby/pay/app/WXPayController.java
+++ BybyMoney_PayData/src/main/java/com/feri/byby/pay/app/WXPayController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
